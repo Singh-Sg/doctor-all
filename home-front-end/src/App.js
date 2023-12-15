b28fdbf8548c870cb3f468488a738e45924d0f93
Deependra Singh <deependrasg@gmail.com>
2023-12-15 17:50:25 +0530

    add app file

--- home-front-end/src/App.js
+++ home-front-end/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './component/Home/Home';
+import Features from './component/Features/Features'
+import Pricing from './component/Pricing/Pricing'
+import ForPatients from './component/ForPatients/ForPatients'
+import Blog from './component/Blog/Blog'
+import SignUp from './component/components/SignUp/Signup'
+import Header from './component/Header/Header';
+import SignIn from './component/components/SignIn/SignIn';
 
 function App() {
+  const [editorData, setEditorData] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/features' element={<Features />}></Route>
+            <Route path='/pricing' element={<Pricing />}></Route>
+            <Route path='/patient' element={<ForPatients />}></Route>
+            <Route path='/blog' element={<Blog />}></Route>
+            <Route path='/signup' element={<SignUp />}></Route>
+            <Route path='/signin' element={<SignIn />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
+    </div >
   );
 }
 
