d24e68b841e1432fb4f40dd074bdf678fa28d2ad
Deependra Singh <deependrasg@gmail.com>
2024-02-14 15:11:14 +0530

    change calender and prev doctorslots file

--- doctor-dashboard/src/Components/Calender/Calender.js
+++ doctor-dashboard/src/Components/Calender/Calender.js
@@ -26,21 +26,6 @@ export default function Calender() {
                             <div>
                                 <Typography paragraph>A calendar is an essential tool for keeping track of appointments and schedules, and it becomes even more critical when it comes to medical appointments. As a patient, it is crucial to stay organized and ensure that you don't miss any of your doctor's appointments. </Typography>
                                 <br />
-                                {/* <Typography paragraph>The content for a calendar must include specific details related to your medical appointments to help you stay on top of
-                                your health. One of the essential pieces of information to include is the date when the doctor is assigning
-                                your appointment. This date is crucial as it helps you plan your schedule accordingly and make necessary
-                                arrangements to attend the appointment. It also allows you to keep track of how frequently you are visiting
-                                the doctor and if there are any changes in your treatment plan. Additionally, including the date when the
-                                doctor is assigning your appointment serves as a reminder for when you need to follow up with your doctor
-                                after a previous appointment. This is especially helpful for patients with chronic conditions who require
-                                regular check-ups and follow-ups with their doctors. Moreover, having this information on your calendar
-                                can also help you plan for any necessary lab tests or diagnostic procedures that may need to be done before
-                                your appointment. It allows you to schedule these tests in advance, ensuring that you have all the necessary
-                                information ready for your doctor's visit. In the case of emergency appointments, knowing the date when the
-                                doctor is assigning your appointment can help you prioritize and reschedule any conflicting events or appointments.
-                                It also serves as a reference point in case you need to reschedule your appointment. Overall, including the date
-                                when the doctor is assigning your appointment in your calendar content is crucial for effective time management,
-                                efficient communication with your doctor, and staying on top of your health.</Typography> */}
                             </div>
                         </Grid>
                         <Grid item xs={12} md={6} lg={6}>
